a80c064aa63de466e9dc0fd52e8d3626632dfcd7
Fadee kannah <fadeekannah@gmail.com>
2018-11-27 21:50:45 -0800

    Document and clean up cpuLinux logic

--- util/cpuLinux.js
+++ util/cpuLinux.js
@@ -2,27 +2,28 @@ const os = require('os');
 
 function cpuLoad() {
     return new Promise((resolve) => {
-        const stats1 = getCPUInfo();
-        const startIdle = stats1.idle;
-        const startTotal = stats1.total;
-
+        // get cpu base timings
+        const base = getCPUInfo();
+        // wait a second
         setTimeout(function() {
-            const stats2 = getCPUInfo();
-            const endIdle = stats2.idle;
-            const endTotal = stats2.total;
-
-            const idle 	= endIdle - startIdle;
-            const total 	= endTotal - startTotal;
-            const perc	= idle / total * 100;
-            console.log(perc)
-            return resolve(perc)
+            // sample current
+            const current = getCPUInfo();
+            // calculate difference between the current and the base cpu timings
+            const idle 	= current.idle - base.idle;
+            const total = current.total - base.total;
+            // calculate percentage
+            const percentage = idle === 0 ? 100 : idle / total * 100;
+            console.log(percentage)
+            return resolve(percentage)
         }, 1000 );
     })
 }
 
 function getCPUInfo(){
+    // get cpus information from the OS
     const cpus = os.cpus();
 
+    // initialize variables to track the different cpu timings
     let user = 0;
     let nice = 0;
     let sys = 0;
@@ -30,6 +31,7 @@ function getCPUInfo(){
     let irq = 0;
     let total = 0;
 
+    // go through all cpus and add up the timings
     for(var cpu in cpus){
         if (!cpus.hasOwnProperty(cpu)) continue;
         user += cpus[cpu].times.user;
@@ -39,12 +41,11 @@ function getCPUInfo(){
         idle += cpus[cpu].times.idle;
     }
 
+    // calculate the total cpu timings
     total = user + nice + sys + idle + irq;
 
-    return {
-        'idle': idle,
-        'total': total
-    };
+    // return total and idle
+    return {idle, total};
 }
 
 // export the function to be able to call it from other files
